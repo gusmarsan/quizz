04d356d29504169ee629f0b0380d1227e4e04823
gusmarsan <gustavomarsan@gmail.com>
2026-08-06 09:54:58 -0300

    Reset stale episode queue before instant rotation starts

--- ai-bootstrap.js
+++ ai-bootstrap.js
@@ -2,4 +2,5 @@ import "./quality-runtime.js";
 import "./episode-intro.js";
 import "./episode-sync.js";
 import "./block-runtime.js";
+import "./episode-migration.js";
 import "./episode-engine.js";
