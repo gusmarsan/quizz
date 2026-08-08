77e44aeae2304b7526b2d0d1c00b3afd41578897
gusmarsan <gustavomarsan@gmail.com>
2026-08-08 13:04:29 -0300

    Apply mobile viewport lock

--- pwa-runtime.js
+++ pwa-runtime.js
@@ -1,3 +1,8 @@
+const mobileViewportStyles = document.createElement("link");
+mobileViewportStyles.rel = "stylesheet";
+mobileViewportStyles.href = "./mobile-viewport-lock-v081.css?v=0.8.1";
+document.head.appendChild(mobileViewportStyles);
+
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
